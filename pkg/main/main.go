06c6ad5b8bcdb1e88546ffab7e7de2f6aec4519e
Lcuas Daniel <lucas.xavier@picpay.com>
2021-08-13 13:28:44 -0300

    Initial commit

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
@@ -11,12 +12,16 @@ func main() {
 	var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
 	var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
 
-	port := ":8080"
+	dotEnvErr := godotenv.Load("./config/.env")
+
+	if dotEnvErr != nil {
+		Error.Fatal("Error loading .env file : ", dotEnvErr)
+	}
 
 	Info.Println("Artemis API")
 
 	router := mux.NewRouter()
-	err := http.ListenAndServe(port, router)
+	err := http.ListenAndServe(os.Getenv("ARTEMIS_PORT"), router)
 
 	if err != nil {
 		Error.Fatal("Can not start Artemis API: ", err)
