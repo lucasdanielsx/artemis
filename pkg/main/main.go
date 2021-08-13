30c994c88756e60b99ea9bfd3b074d8dc24dd6b5
Lcuas Daniel <lucas.xavier@picpay.com>
2021-08-13 12:17:26 -0300

    Initial commit

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
+	var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
+
+	port := ":8080"
+
+	Info.Println("Artemis API")
+
+	router := mux.NewRouter()
+	err := http.ListenAndServe(port, router)
+
+	if err != nil {
+		Error.Fatal("Can not start Artemis API: ", err)
+	}
 }
