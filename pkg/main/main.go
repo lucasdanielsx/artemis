dce7315c2905514b698ff160ef5960757ea6a534
Lcuas Daniel <lucas.xavier@picpay.com>
2021-08-13 13:38:09 -0300

    Change dot env lib

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 	"os"
@@ -12,16 +12,19 @@ func main() {
 	var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
 	var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
 
-	dotEnvErr := godotenv.Load("./config/..env")
+	viper.SetConfigFile("./config/.env")
+	dotEnvErr := viper.ReadInConfig()
 
 	if dotEnvErr != nil {
 		Error.Fatal("Error loading ..env file : ", dotEnvErr)
 	}
 
+	port, _ := viper.Get("ARTEMIS_PORT").(string)
+
 	Info.Println("Artemis API")
 
 	router := mux.NewRouter()
-	err := http.ListenAndServe(os.Getenv("ARTEMIS_PORT"), router)
+	err := http.ListenAndServe(port, router)
 
 	if err != nil {
 		Error.Fatal("Can not start Artemis API: ", err)
