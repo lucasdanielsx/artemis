7d3481a2d39e56e4c6aad7581400d1cab7542894
Lcuas Daniel <lucas.xavier@picpay.com>
2021-08-13 13:31:05 -0300

    Initial commit

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -12,10 +12,10 @@ func main() {
 	var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
 	var Info = log.New(os.Stdout, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
 
-	dotEnvErr := godotenv.Load("./config/.env")
+	dotEnvErr := godotenv.Load("./config/..env")
 
 	if dotEnvErr != nil {
-		Error.Fatal("Error loading .env file : ", dotEnvErr)
+		Error.Fatal("Error loading ..env file : ", dotEnvErr)
 	}
 
 	Info.Println("Artemis API")
