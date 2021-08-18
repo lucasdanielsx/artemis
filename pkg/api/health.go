199eaa035a6a41fcfef1dbd66c1be22783591a41
Lcuas Daniel <lucas.xavier@picpay.com>
2021-08-18 18:00:54 -0300

    Updates on health

--- pkg/api/health.go
+++ pkg/api/health.go
@@ -2,16 +2,38 @@ package api
 
 import (
 	database "artemis/pkg/database"
+	"artemis/pkg/util"
+	"encoding/json"
 	"net/http"
 )
 
+type health struct {
+	Name   string `json:"name"`
+	Status bool   `json:"status"`
+}
+
 func Health(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var checks []health
+
 	redis := database.RedisHealth()
 	postgres := database.PostgresHealth()
 
-	if redis && postgres {
+	checks = append(checks, health{"redis", redis})
+	checks = append(checks, health{"postgres", postgres})
+
+	if !redis || !postgres {
+		checks = append(checks, health{"app", false})
+		w.WriteHeader(503)
+	} else {
+		checks = append(checks, health{"app", true})
 		w.WriteHeader(200)
 	}
 
-	w.WriteHeader(500)
+	marshal, err := json.Marshal(checks)
+	util.HandleError("Can not marshal checks", err)
+
+	_, err = w.Write(marshal)
+	util.HandleError("Can not encode checks", err)
 }
